55e2deba54aabe885f57caf7a8dc2b5db9a57a88
Eduardo Marques <programdor.eduardo@gmail.com>
2022-11-26 07:41:04 -0300

    first steps

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,15 @@
 // 1 - iniciando projeto
-console.log("Express + TS!!!");
+//console.log("Express + TS!!!");
+
+//  init express
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    return res.send("Hello express!")
+})
+
+app.listen(3000,()=>{
+    console.log("Aplicação de TS + Express funcionando!")
+});
